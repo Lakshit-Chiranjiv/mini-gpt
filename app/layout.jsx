dd3aa1479e6fa8d41f13edd32bae7ba865573074
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-06-29 23:54:32 +0530

    added main tag in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <main className="flex min-h-screen flex-col items-center justify-between p-6 md:py-12 md:px-24">
+          {children}
+        </main>
       </body>
     </html>
   );
