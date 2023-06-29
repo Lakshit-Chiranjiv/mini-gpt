d58812a56cbb380b4139f7c0440d7ec5e2403b77
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-06-29 23:54:49 +0530

    added animation

--- app/components/MacWindow.jsx
+++ app/components/MacWindow.jsx
@@ -4,9 +4,9 @@ const MacWindow = ({children}) => {
     <div className="w-full border border-stone-400 rounded-lg">
       <div className="border-b border-stone-400 h-12 flex justify-start">
         <div className="flex justify-center items-center gap-3 mx-4">
-          <div className="bg-red-400 w-3 h-3 rounded-full"></div>
-          <div className="bg-yellow-400 w-3 h-3 rounded-full"></div>
-          <div className="bg-green-400 w-3 h-3 rounded-full"></div>
+          <div className="bg-red-400 w-3 h-3 rounded-full animate-bounce"></div>
+          <div className="bg-yellow-400 w-3 h-3 rounded-full animate-bounce-late-1"></div>
+          <div className="bg-green-400 w-3 h-3 rounded-full animate-bounce-late-2"></div>
         </div>
       </div>
 
