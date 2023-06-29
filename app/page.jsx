c629dc9ae23a03db4db81bac03d6a3ead0a16a2b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-06-30 00:24:22 +0530

    added input and btn

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,15 @@ import MacWindow from "./components/MacWindow";
 
 export default function Home() {
   return (
-      <MacWindow>
-        Home
-      </MacWindow>
+    <MacWindow>
+      <textarea
+        placeholder="Lets Discuss..."
+        className="input input-bordered input-lg w-full"
+      ></textarea>
+
+      <button className="btn btn-success btn-outline my-4 w-full">
+        Submit
+      </button>
+    </MacWindow>
   );
 }
