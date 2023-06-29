e3762b3d1eae06a2cbc2b474100a92bab49e4c30
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-06-29 23:55:04 +0530

    home page cleaned

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,9 @@
+import MacWindow from "./components/MacWindow";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6 md:py-12 md:px-24">
-
-      <div className="w-full border border-stone-400 rounded-lg">
-
-        <div className="border-b border-stone-400 h-12 flex justify-start">
-          <div className="flex justify-center items-center gap-3 mx-4">
-            <div className="bg-red-400 w-3 h-3 rounded-full"></div>
-            <div className="bg-yellow-400 w-3 h-3 rounded-full"></div>
-            <div className="bg-green-400 w-3 h-3 rounded-full"></div>
-          </div>
-        </div>
-
-        <div className="p-6">
-          hello
-        </div>
-      </div>
-    </main>
+      <MacWindow>
+        Home
+      </MacWindow>
   );
 }
