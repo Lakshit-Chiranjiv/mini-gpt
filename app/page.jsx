25db4995324b1cfd56c87a5f6ee3d9ccd1c6a090
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-07-23 22:52:33 +0530

    defined states and passed data with link

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,27 @@
+'use client';
 import MacWindow from "./components/MacWindow";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+
+  const [userInput, setUserInput] = useState('')
+
   return (
     <MacWindow>
       <textarea
-        placeholder="Lets Discuss..."
-        className="input input-bordered input-lg w-full"
+        placeholder="What you wanna know about?"
+        className="input input-bordered input-lg w-full h-36"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
       ></textarea>
 
-      <button className="btn btn-success btn-outline my-4 w-full">
+      <Link className="btn btn-success btn-outline my-4 w-full" href={{
+        pathname: '/chat',
+        query: { userInput }
+      }}>
         Submit
-      </button>
+      </Link>
     </MacWindow>
   );
 }
