31c7f4fd452baef3bf7ec9873ff9672f5fbaaac9
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-07-23 12:29:41 +0530

    font class apply

--- app/components/MacWindow.jsx
+++ app/components/MacWindow.jsx
@@ -1,7 +1,7 @@
 
 const MacWindow = ({children}) => {
   return (
-    <div className="w-full border border-stone-400 rounded-lg">
+    <div className="w-full border border-stone-400 rounded-lg font-poppins">
       <div className="border-b border-stone-400 h-12 flex justify-start">
         <div className="flex justify-center items-center gap-3 mx-4">
           <div className="bg-red-400 w-3 h-3 rounded-full animate-bounce"></div>
